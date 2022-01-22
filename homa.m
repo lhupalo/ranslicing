62edfa6d0b4bea102541437ca801da97dfcc2ab9
lhupalo <lhpzl@protonmail.com>
2022-01-22 14:00:22 -0300

    add fixes

--- homa.m
+++ homa.m
@@ -27,30 +27,32 @@ iter = 10e3;
 n_outage = [];
 
 
+SINR = [];
 for i = 1:iter
 
     H = sqrt(10^(SNR_M/10))*((randn(Am,1))+1i*randn(Am,1))/sqrt(2); % Gera vetor H de canais para cada usuário mMTC
     %H = normrnd(0,sqrt(Gamma_M),Am)+i*normrnd(0,sqrt(Gamma_M),Am);
-    H_index = [1:1:Am];
-    G = abs(H).^2;
-    
+    G_dB = abs(H).^2;
+    G = 10.^(G_dB/10);
     
     ok = 0;
     Dm = 0;
+    n_outage = [];
+    n_ok = [];
     for j = 1:Am
 
         G_sorted = sort(G,'descend');
 
         Gsum = sum(G_sorted(j+1:Am,1));
-        SINR = G_sorted(j,1)/(1+Gsum);
+        SINR(j,1) = G_sorted(j,1)/(1+Gsum);
         
-        if log2(1 + SINR) <= Rm
+        if log2(1 + SINR(j,1)) <= Rm
             ok = ok + 1;
         else
             Dm = Am - ok;
-            break;
         end
-        
     end
-    
+    n_ok = [n_ok ok];
+    n_outage = [n_outage Dm];
 end
+lambda = mean(ok)
